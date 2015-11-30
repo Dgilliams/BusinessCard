97c09b6005a6dbaab29d6e1c83b81e39953e2000
Damo <Damosdesigns@gmail.com>
2015-11-29 23:11:01 -0500

    successful git tests

--- app/src/main/java/com/example/damo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/damo/myapplication/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRoot = init();
-//commentcommentcomment bring in the turkey!
         initAdMob();
     }
 
