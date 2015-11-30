fa306237f8cb1594ef21509629fb76cf149c70d6
Damien Gilliams <DamosDesigns@gmail.com>
2015-11-29 23:10:05 -0500

    test from macbook air

--- app/src/main/java/com/example/damo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/damo/myapplication/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRoot = init();
-
+//commentcommentcomment bring in the turkey!
         initAdMob();
     }
 
